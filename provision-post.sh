839c8d27185a20d6243a5239e18ff5ec03dd17e0
Gabriele Baldassarre <gabriele.baldassarre@gmail.com>
2017-07-18 15:00:16 +0200

    add slug for woc

--- provision-post.sh
+++ provision-post.sh
@@ -2,5 +2,6 @@
 
 set -ex
 
-/usr/local/bin/wp --path=/var/www/wordpress site create --slug=eden --title="Eden: Deception"
-/usr/local/bin/wp --path=/var/www/wordpress --url="http://9thcircle.dev/eden" theme activate eden-frontend
+/usr/local/bin/wp --path=/var/www/html site create --slug=eden --title="Eden: Deception"
+/usr/local/bin/wp --path=/var/www/html site create --slug=woc --title="Wisdom of Cthulhu"
+#/usr/local/bin/wp --path=/var/www/html --url="http://9thcircle.dev/eden" theme activate eden-frontend
